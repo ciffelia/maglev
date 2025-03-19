2caed1f942a60dc16f6819e2cd70e7e931fe3649
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-03-19 23:08:42 +0900

    chore: tweak eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,55 @@
 import js from "@eslint/js";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import perfectionist from "eslint-plugin-perfectionist";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import unicorn from "eslint-plugin-unicorn";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "worker-configuration.d.ts"] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       perfectionist.configs["recommended-natural"],
+      unicorn.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
     ],
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      react,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "react/jsx-no-target-blank": "off",
+      "unicorn/better-regex": "error",
+      "unicorn/custom-error-definition": "error",
+      "unicorn/prefer-query-selector": "off",
+      "unicorn/prevent-abbreviations": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 );
