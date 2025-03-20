91c4b73882b9f6dc5195a71c502a1ef8dbc0e6a2
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-03-20 15:19:56 +0900

    introduce drizzle

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,9 +1,29 @@
+import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
+
+import * as schema from "./db/schema";
 
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/ping", (c) => {
   return c.json({ name: "Cloudflare" });
+});
+
+app.get("/api/user", async (c) => {
+  const db = drizzle(c.env.DB);
+  const result = await db.select().from(schema.users).all();
+  return c.json(result);
+});
+
+app.post("/api/user", async (c) => {
+  const params = await c.req.json<typeof schema.users.$inferSelect>();
+  const db = drizzle(c.env.DB);
+
+  const result = await db.insert(schema.users).values({
+    name: params.name,
+  });
+
+  return c.json(result);
 });
 
 export default app satisfies ExportedHandler<Env>;
